f6244407e038e5c7a6e8d6a3f7563da8b89324f8
marcossssantos1 <marcossantosmsms604@gmail.com>
2025-01-27 16:51:51 -0300

    adicionando tratamento de erros na api

--- src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
@@ -1,5 +1,5 @@
-package med.voll.api.medico;
-import med.voll.api.endereco.Endereco;
+package med.voll.api.domain.medico;
+import med.voll.api.domain.endereco.Endereco;
 
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
 
--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,8 +1,8 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
 @Entity(name = "paciente")
 @Table(name = "pacientes")
